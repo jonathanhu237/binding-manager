8a732f3186c4b59df0e588b7442428b39340b3cd
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-20 16:12:19 +0000

    feat: add migration to create user table
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

---
+++ backend/alembic/versions/cb909da56976_create_user_table.py
@@ -0,0 +1,41 @@
+"""create user table
+
+Revision ID: cb909da56976
+Revises:
+Create Date: 2025-07-20 14:05:59.649413
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "cb909da56976"
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.execute("""
+        CREATE EXTENSION IF NOT EXISTS citext;
+
+        CREATE TABLE users (
+            id bigserial PRIMARY KEY,
+            username text UNIQUE NOT NULL,
+            hashed_password bytea UNIQUE NOT NULL,
+            email citext UNIQUE NOT NULL,
+            version integer NOT NULL DEFAULT 1
+        );
+    """)
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.execute("""
+        DROP EXTENSION IF EXISTS citext;
+
+        DROP TABLE IF EXISTS users;
+    """)
