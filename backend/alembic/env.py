d3017f8f0b869baac1330f29d608480150f81b2b
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-20 14:01:16 +0000

    feat: get database uri from .env file instead of alembic.ini
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,9 +1,8 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from app.core.config import settings
+from sqlalchemy import engine_from_config, pool
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -26,6 +25,10 @@ target_metadata = None
 # ... etc.
 
 
+def get_url():
+    return str(settings.postgres_database_uri)
+
+
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
@@ -38,7 +41,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -64,9 +67,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
