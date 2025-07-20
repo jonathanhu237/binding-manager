89e468a83a64816beaff6fc5dfe3d560b21e764c
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-20 15:29:02 +0000

    fix: update configuration retrieval in run_migrations_online
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -60,8 +60,10 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    configuration = config.get_section(config.config_ini_section, {})
+    configuration["sqlalchemy.url"] = get_url()
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
