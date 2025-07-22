9a8f74ced32778af10f9c321927d85fdf5b81733
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-23 00:20:01 +0800

    chore: begin to rewrite the backend using golang
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- backend/app/repository/main.py
+++
@@ -1,8 +0,0 @@
-from psycopg_pool import ConnectionPool
-
-from app.repository.user import UserRepository
-
-
-class Repository:
-    def __init__(self, pool: ConnectionPool):
-        self.user = UserRepository(pool)
--- backend/app/service/main.py
+++
@@ -1,9 +0,0 @@
-from logging import Logger
-from app.core.config import Settings
-from app.repository.main import Repository
-from app.service.user import UserService
-
-
-class Service:
-    def __init__(self, logger: Logger, config: Settings, repository: Repository):
-        self.user = UserService(logger, config, repository)
