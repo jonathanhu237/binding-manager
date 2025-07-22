43e9b8f472f222c83b718a2e07b7aece0560dc13
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-22 19:43:43 +0800

    feat: update settings configuration to remove default values and computed fields
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,37 +1,26 @@
-from pydantic import computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from sqlalchemy import URL
 
 
 class Settings(BaseSettings):
     jwt_secret: str
-    jwt_expire_minutes: int = 7 * 24 * 60
+    jwt_expire_minutes: int
 
-    first_admin_username: str = "admin"
+    first_admin_username: str
     first_admin_password: str
     first_admin_email: str
 
-    postgres_username: str = "binding_manager"
+    postgres_username: str
     postgres_password: str
-    postgres_host: str = "localhost"
-    postgres_port: int = 5432
-    postgres_db: str = "binding_manager"
-
-    @computed_field
-    @property
-    def postgres_database_uri(self) -> str:
-        url = URL.create(
-            "postgresql+psycopg2",
-            username=self.postgres_username,
-            password=self.postgres_password,
-            host=self.postgres_host,
-            port=self.postgres_port,
-            database=self.postgres_db,
-        ).render_as_string(hide_password=False)
-
-        return str(url)
+    postgres_host: str
+    postgres_port: int
+    postgres_db: str
 
     model_config = SettingsConfigDict()
 
 
-settings = Settings()  # type: ignore
+def get_config() -> Settings:
+    return Settings.model_validate({})
+
+
+if __name__ == "__main__":
+    print(get_config().model_dump_json(indent=4))
