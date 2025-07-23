294aee2c4f5109929abfa068c60176a80e448b5d
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-23 23:08:25 +0800

    refactor: migrate user ids from int to uuid
    - Updates the User struct to use uuid.UUID for the Id field.
    - Modifies the database migration to enable the uuid-ossp extension and sets the id column type to uuid.
    - Adds the google/uuid dependency for uuid generation and handling.
    
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,9 +1,13 @@
 package domain
 
+import (
+	"github.com/google/uuid"
+)
+
 type User struct {
-	Id           int64  `json:"id"`
-	Username     string `json:"username"`
-	PasswordHash string `json:"-"`
-	Email        string `json:"email"`
-	Version      int    `json:"version"`
+	Id           uuid.UUID `json:"id"`
+	Username     string    `json:"username"`
+	PasswordHash string    `json:"-"`
+	Email        string    `json:"email"`
+	Version      int       `json:"version"`
 }
--- migrations/000001_create_user_table.up.sql
+++ migrations/000001_create_user_table.up.sql
@@ -1,6 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS citext;
 CREATE TABLE IF NOT EXISTS users (
-    id bigserial PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uudid_generate_v4(),
     username text UNIQUE NOT NULL,
     password_hash bytea UNIQUE NOT NULL,
     email citext UNIQUE NOT NULL,
