1764c52d906051d5d3a05cb7235949b59b105e43
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-23 23:12:45 +0800

    fix: correct typo in migration script
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- migrations/000001_create_user_table.up.sql
+++ migrations/000001_create_user_table.up.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS citext;
 CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY DEFAULT uudid_generate_v4(),
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     username text UNIQUE NOT NULL,
     password_hash bytea UNIQUE NOT NULL,
     email citext UNIQUE NOT NULL,
