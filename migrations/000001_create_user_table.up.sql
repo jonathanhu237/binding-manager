94b9e8bd9a00baafc5b61bf62868bf78200c94f2
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-25 00:29:41 +0800

    fix: remove the unique constrain from password_hash column in users table
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- migrations/000001_create_user_table.up.sql
+++ migrations/000001_create_user_table.up.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS citext;
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     username text UNIQUE NOT NULL,
-    password_hash bytea UNIQUE NOT NULL,
+    password_hash bytea NOT NULL,
     email citext UNIQUE NOT NULL,
     is_admin boolean NOT NULL DEFAULT FALSE,
     version integer NOT NULL DEFAULT 1
