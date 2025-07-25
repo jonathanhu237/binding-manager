288acb33469b89ec91cd576e634741c9e776ac6a
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-25 16:27:09 +0800

    refactor: remove email column and associated citext extension from users table
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- migrations/000001_create_user_table.up.sql
+++ migrations/000001_create_user_table.up.sql
@@ -1,10 +1,8 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE EXTENSION IF NOT EXISTS citext;
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     username text UNIQUE NOT NULL,
-    password_hash bytea NOT NULL,
-    email citext UNIQUE NOT NULL,
+    password_hash text NOT NULL,
     is_admin boolean NOT NULL DEFAULT FALSE,
     version integer NOT NULL DEFAULT 1
 );
