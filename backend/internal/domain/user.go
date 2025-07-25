7b4c96cce6ff0f5bb4d388626c67b15023495840
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-25 16:31:02 +0800

    refactor: move the password related logic to user.go
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- backend/internal/domain/user.go
+++ backend/internal/domain/user.go
@@ -2,12 +2,29 @@ package domain
 
 import (
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	Id       uuid.UUID `json:"id"`
-	Username string    `json:"username"`
-	Password Password  `json:"-"`
-	IsAdmin  bool      `json:"is_admin"`
-	Version  int       `json:"version"`
+	Id           uuid.UUID    `json:"id"`
+	Username     string       `json:"username"`
+	PasswordHash PasswordHash `json:"-"`
+	IsAdmin      bool         `json:"is_admin"`
+	Version      int          `json:"version"`
+}
+
+type PasswordHash string
+
+func GeneratePasswordHash(plaintext string) (PasswordHash, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintext), bcrypt.DefaultCost)
+
+	if err != nil {
+		return "", err
+	}
+
+	return PasswordHash(hash), nil
+}
+
+func (ph *PasswordHash) Verify(plaintext string) error {
+	return bcrypt.CompareHashAndPassword([]byte(*ph), []byte(plaintext))
 }
