6c2211b4a35e0d5bb4704a0d37624332f3af86fa
jonathanhu237 <jonathanhu237@gmail.com>
2025-07-25 13:20:25 +0800

    feat: implement readJSON function for handling JSON requests
    Signed-off-by: jonathanhu237 <jonathanhu237@gmail.com>

--- backend/internal/apiserver/json.go
+++ backend/internal/apiserver/json.go
@@ -14,3 +14,13 @@ func (as *ApiServer) writeJson(w http.ResponseWriter, status int, data any, head
 
 	return json.NewEncoder(w).Encode(data)
 }
+
+func (as *ApiServer) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+	maxBytes := 1_048_576 // 1 MB
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	return dec.Decode(dst)
+}
